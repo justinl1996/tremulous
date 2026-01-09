d1d0be56f6ea57a7da48befeef33dffd3680d9ae
Auriga <aurigasyno@gmail.com>
2026-01-09 00:11:39 -0700

    Missing return values?

--- src/qcommon/common.cpp
+++ src/qcommon/common.cpp
@@ -3934,11 +3934,11 @@ Bucket Selection System
 */
 
 static void *Z_PlaceHolderAlloc(int size) {
-    Z_Malloc(size);
+    return Z_Malloc(size);
 }
 
 unsigned int Com_Bucket_Create_Bucket(void) {
-    Q_Bucket_Create_Bucket(Z_PlaceHolderAlloc, Z_Free);
+    return Q_Bucket_Create_Bucket(Z_PlaceHolderAlloc, Z_Free);
 }
 
 void Com_Bucket_Delete_Bucket(unsigned int bucket_handle) {
